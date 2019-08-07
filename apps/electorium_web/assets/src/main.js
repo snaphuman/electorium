f56592197d56d2824c4438c59ac5d6ab18330983
Fabian Hernandez <fhernandezn@gmail.com>
2019-08-07 07:37:22 -0500

    [deps] add buefy as user interface component library

--- apps/electorium_web/assets/src/main.js
+++ apps/electorium_web/assets/src/main.js
@@ -1,23 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/index.js'
-import './../node_modules/bulma/css/bulma.css'
+import Vue from 'vue';
+import Buefy from 'buefy';
+import App from './App.vue';
+import router from './router';
+import store from './store/index.js';
+import 'buefy/dist/buefy.css';
 import 'vue-flash-message/dist/vue-flash-message.min.css';
 import VueFlashMessage from 'vue-flash-message';
 
+Vue.use(Buefy);
 Vue.use(VueFlashMessage);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
 
-//new Vue({
-//  router,
-//  el: '#app',
-//  template: '<App/>',
-//  components: { App }
-//})
