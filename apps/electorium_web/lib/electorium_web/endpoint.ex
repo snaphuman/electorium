121383e5965bc25ab484eab402ffb8fa6d98aeec
arpunk <arpunk@fatelectron.net>
2020-02-01 11:49:17 -0500

    Update dependencies

--- apps/electorium_web/lib/electorium_web/endpoint.ex
+++ apps/electorium_web/lib/electorium_web/endpoint.ex
@@ -1,6 +1,15 @@
 defmodule ElectoriumWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :electorium_web
 
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_electorium_web_key",
+    signing_salt: "Lp16Gihn"
+  ]
+
   socket "/socket", ElectoriumWeb.UserSocket,
     websocket: true,
     longpoll: false
@@ -24,7 +33,7 @@ defmodule ElectoriumWeb.Endpoint do
   end
 
   plug Plug.RequestId
-  plug Plug.Logger
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
@@ -33,14 +42,6 @@ defmodule ElectoriumWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_electorium_web_key",
-    signing_salt: "Lp16Gihn"
-
+  plug Plug.Session, @session_options
   plug ElectoriumWeb.Router
 end
