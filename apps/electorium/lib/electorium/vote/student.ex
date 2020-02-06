13773732fbd84e4f7f07bff6df59133180f0eb62
snaphuman <fhernandezn@gmail.com>
2020-02-06 17:21:47 -0500

    [model] define student schema and changeset

--- apps/electorium/lib/electorium/vote/student.ex
+++ apps/electorium/lib/electorium/vote/student.ex
@@ -2,16 +2,32 @@ defmodule Electorium.Vote.Student do
   use Ecto.Schema
   import Ecto.Changeset
 
+
   schema "students" do
+    field :publicKey, :string
+    field :secretKey, :string
     field :name, :string
+    field :last_name, :string
+    field :email, :string
+    field :course, :string
+    field :isCandidate, :boolean
 
     timestamps()
   end
 
   @doc false
-  def changeset(student, attrs) do
+  def changeset(student, attrs \\ %{}) do
     student
-    |> cast(attrs, [:name])
-    |> validate_required([:name])
+    |> cast(attrs, [:publicKey,
+                   :privateKey,
+                   :name,
+                   :last_name,
+                   :email,
+                   :course,
+                   :isCandidate])
+    |> validate_required([:name,
+                         :email,
+                         :publicKey,
+                         :secretKey])
   end
 end
